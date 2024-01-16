4958ca516fefb64b01a5ec511f424f643d5ae694
Meriç Türkyılmaz <mrctrkylmzq@gmail.com>
2024-01-16 21:15:40 +0300

    01062024

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsService } from './reports.service';
+import { ReportsController } from './reports.controller';
+import { Report } from './report.entity';
 
 @Module({
-  providers: [ReportsService]
+  imports:[TypeOrmModule.forFeature([Report])],
+  providers: [ReportsService],
+  controllers: [ReportsController]
 })
 export class ReportsModule {}
